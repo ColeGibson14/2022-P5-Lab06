6ce09417499dc2b8ece6d600cf3bceb44a1a1e31
Cole Gibson <Cole.Gibson@vtsd.com>
2022-11-16 12:17:26 -0500

    90 Completed Lab

--- Lab06vst.java
+++ Lab06vst.java
@@ -9,35 +9,51 @@ import java.util.*;
 public class Lab06vst extends Applet
 {
 
-    public void paint(Graphics g)
-    {
+    public void paint(Graphics g) {
         // Draw Grid
-        g.drawRect(10,10,780,580);
-        g.drawLine(400,10,400,590);
-        g.drawLine(10,300,790,300);
-
+        g.drawRect(10, 10, 780, 580);
+        g.drawLine(400, 10, 400, 590);
+        g.drawLine(10, 300, 790, 300);
 
 
         // Draw Random Lines
-
-
-
-
-
+        Random rand = new Random(7442);
+        for (int k = 0; k <= 100; k++) {
+            int x1 = rand.nextInt(390) + (10);
+            int y1 = rand.nextInt(280) + (10);
+            int x2 = rand.nextInt(390) + (10);
+            int y2 = rand.nextInt(280) + (10);
+            int c1 = rand.nextInt(255);
+            int c2 = rand.nextInt(255);
+            int c3 = rand.nextInt(255);
+            g.drawLine(x1, y1, x2, y2);
+            g.setColor(new Color(c1,c2,c3));
+        }
         // Draw Random Squares
+        for(int k = 0; k<= 100; k++){
+        int rx1 = (int) (Math.random() * 331 + 400);
+        int ry1 = (int) (Math.random() * 231 + 10);
+        int width = 50;
+        int height = 50;
+        int c1 = rand.nextInt(255);
+        int c2 = rand.nextInt(255);
+        int c3 = rand.nextInt(255);
+            g.fillRect(rx1, ry1, width, height);
+            g.setColor(new Color(c1,c2,c3));
 
-
-
-
-
+         }
         // Draw Random Circles
-
-
-
-
-
-        // Draw 3-D Box
-
+        for(int k = 0; k<= 100; k++){
+            int diameter = rand.nextInt(200);
+            int ox1 = rand.nextInt(390 - diameter)+10;
+            int oy1 = rand.nextInt(290 - diameter)+300;
+            int c1 = rand.nextInt(255);
+            int c2 = rand.nextInt(255);
+            int c3 = rand.nextInt(255);
+            g.drawOval(ox1,oy1,diameter, diameter);
+            g.setColor(new Color(c1,c2,c3));
+        }
+            // Draw 3-D Box
 
 
 
